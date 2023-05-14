d2bd799072665c11d4fb75d85c6bca70b501e4e8
Marc C <mcar176@gmail.com>
2023-05-14 20:37:56 +0200

    env var is messing with netlify

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-import * as dotenv from "dotenv";
-
 import App from "./App.jsx";
 import AuthProvider from "./components/authProvider";
-
-dotenv.config();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,3 @@
 // export const
 export const API_URL =
-  process.env.API_URL || "https://site--marvel-back--dqr6xggsg6hp.code.run";
+  import.meta.env.API_URL || "https://site--marvel-back--dqr6xggsg6hp.code.run";
