c6adbed6c49bf37c5684dc609fcdcfce263dce48
Marc C <mcar176@gmail.com>
2023-05-14 20:23:56 +0200

    Fix imports for Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import * as dotenv from "dotenv";
-dotenv.config();
 
 import App from "./App.jsx";
 import AuthProvider from "./components/authProvider";
+
+dotenv.config();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
